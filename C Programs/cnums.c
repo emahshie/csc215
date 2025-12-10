5aa5f270ae0844c8f7dfd85012e6e1b04dfb30b7
Eleanor Mahshie <ella@mahshie.net>
2025-12-10 09:25:20 -0500

    Add null termination for final line in cnums.c
    Ensure the final partial line is null-terminated before printing.

--- C Programs/cnums.c
+++ C Programs/cnums.c
@@ -1,3 +1,4 @@
+/*Eleanor Mahshie*/
 #include <stdio.h>
 
 #define MAX_ARR_SIZE 80
@@ -62,6 +63,7 @@ char* argv[];
     }
 
     if (chrCount) {
+        line[chrCount] = '\0';  /* <-- Added: ensure the final partial line is null-terminated */
         printf("%d:%s%s\n", chrCount, multipleChr(' ', MAX_SPACE - getDigits(chrCount)), line);
     }
 
