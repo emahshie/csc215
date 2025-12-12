fc8b435da00ddd1ad38887ceeb9f3c4e40e51d6b
Eleanor Mahshie <ella@mahshie.net>
2025-12-12 09:49:48 -0500

    Correct atoi function implementation
    Fix variable initialization and formatting in atoi function.

--- C Programs/atoi.c
+++ C Programs/atoi.c
@@ -1,13 +1,13 @@
-\* Eleanor Mahshie *\
+/* Eleanor Mahshie */
 
-int *atoi(char *t){
-char *s;
-s = t;
-  
-int total;
-  while (*s >= '0' && *s <= '9'){
-    total = 10 * n + *s - '0';
-    s++;
-  }
-return total;
+int atoi(char *t){
+  char *s;
+  s = t;
+    
+  int total;
+    while (*s >= '0' && *s <= '9'){
+      total = 10 * total + *s - '0';
+      s++;
+    }
+  return total;
 }
