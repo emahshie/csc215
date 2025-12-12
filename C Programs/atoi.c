a47740ed713689ed4a549e658fbf9c31ffa61321
Eleanor Mahshie <ella@mahshie.net>
2025-12-12 10:32:36 -0500

    add signs
    Added sign handling for negative and positive integers.

--- C Programs/atoi.c
+++ C Programs/atoi.c
@@ -3,13 +3,22 @@
 int atoi(char *t){
   char *s;
   s = t;
-    
+  int sign;
+  sign = 1;
+  
   int total;
   total = 0;
+
+  if (*s == '-' || *s == '+'){
+    if (*s == '-')
+            sign = -1;
+        s++;
+    }
+    
   
     while (*s >= '0' && *s <= '9'){
       total = 10 * total + (*s - '0');
       s++;
     }
-  return total;
+  return (sign * total);
 }
