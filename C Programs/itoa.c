4bf54a352d59f8ac7bc70378b25fbc37e4279877
Eleanor Mahshie <ella@mahshie.net>
2025-12-12 10:42:16 -0500

    write
    Fix itoa function to handle negative numbers and add sign support.

--- C Programs/itoa.c
+++ C Programs/itoa.c
@@ -1,8 +1,20 @@
 /* Eleanor Mahshie */
 char itoa(int n, int t){
   char *s;
-  
+  char sign;
 
-
-
+  if (n < 0){
+    sign = -1;
+    n = -n;
+  }
+  while ((n /=10) > 0) {
+    *s = n % 10 + '0';
+    s++;
+    }
+  if (sign == -1){
+    *s = '-';
+    s++;
+      }
+  s = '\0';
+  reverse(s);
 }
