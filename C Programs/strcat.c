2e79f4bf58d13ebe5590650888f960b369db6fac
Eleanor Mahshie <ella@mahshie.net>
2025-12-12 09:46:39 -0500

    Refactor strcat function implementation
    Refactored strcat function to use proper parameter types and return value.

--- C Programs/strcat.c
+++ C Programs/strcat.c
@@ -1,12 +1,15 @@
 /* Eleanor Mahshie */
 #include <stdio.h>
 
-strcat(s, t) 
-char *s, *t; 
-{
-
-  while (*s != '\0')
-    s++;
-  while ((*s = *t != '\0')
-    ;
+char *strcat(char *x, char *y) {
+  char *s, *t; 
+  s = x;
+  t = y;
+  
+  
+    while (*s != '\0')
+      s++;
+    while ((*s = *t != '\0')
+      ;
+  return s;
 }
