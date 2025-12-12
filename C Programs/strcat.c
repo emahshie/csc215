fd4d1ef254aed91cd1fc26e7164b92731aaf2c75
Eleanor Mahshie <ella@mahshie.net>
2025-12-12 09:38:49 -0500

    Refactor strcat function in strcat.c
    Refactor strcat function to improve implementation.

--- C Programs/strcat.c
+++ C Programs/strcat.c
@@ -1,11 +1,10 @@
 /* Eleanor Mahshie */
 #include <stdio.h>
-#include "string.h"
+
 strcat(s, t) 
 char *s, *t; 
 {
-  int i, j;
-  i = j = 0;
+
   while (*s != '\0')
     s++;
   while ((*s = *t != '\0')
