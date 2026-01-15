d2da7b8a0353816d1fcaf08aad0c4dc43cbeb9b4
Mahshie <ella@mahshie.net>
2026-01-15 10:04:49 -0500

    print

--- CPrograms/linkList/linklist.c
+++ CPrograms/linkList/linklist.c
@@ -9,3 +9,42 @@ struct node *mknode(n) int n; {
     x->next = NULL;
     return x;
 }
+
+char* prnlist(list, list_str)
+struct node* list;
+char* list_str;
+{
+    struct node* p;
+    char* ptr;
+    char numbuf[10];
+    int i, num, neg;
+    
+    ptr = list_str;
+    p = list;
+    *ptr++ = '[';
+    
+    while (p != NULL) {
+        num = p->num;  
+        neg = num < 0;
+        if (neg) num = -num; 
+        
+        i = 0;
+        do {
+            numbuf[i++] = '0' + (num % 10);
+            num /= 10;
+        } while (num > 0);
+        
+        if (neg) numbuf[i++] = '-';
+        while (--i >= 0) *ptr++ = numbuf[i];
+        
+        if (p->next != NULL) {
+            *ptr++ = ' '; *ptr++ = '-'; *ptr++ = '>';
+            *ptr++ = ' ';
+        }
+        p = p->next;
+    }
+    
+    *ptr++ = ']';
+    *ptr = '\0';
+    return list_str;
+}
--- CPrograms/linkList/linklisttest.c
+++ CPrograms/linkList/linklisttest.c
@@ -5,12 +5,27 @@
 main() {
   START_TESTING("LINKLIST.C");
 
-  TEST_CASE("Test can create node") {
-      struct node* mynode;
-      mynode = mknode(42);
-      ASSERT(mynode->num == 42);
-      ASSERT(mynode->next == NULL);
-  }
+    TEST_CASE("Test can create node") {
+        struct node* mynode;
+        mynode = mknode(42);
+        ASSERT(mynode->num == 42);
+        ASSERT(mynode->next == NULL);
+    }
+
+    TEST_CASE("Test print list") {
+        struct node* head;
+        struct node* second;
+        struct node* third;
+        char list_str[100];
+    
+        head = mknode(1);
+        second = mknode(2);
+        third = mknode(3);
+    
+        head->next = second;
+        second->next = third;
+        ASSERT_STR(prnlist(head, list_str), "[1 -> 2 -> 3]");
+    }
 
   END_TESTING();
 }
